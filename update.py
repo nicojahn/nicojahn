0c302693c5bd4a37aefd04a73ae304fc511c3cc8
Nico Jahn <dev@nicojahn.com>
2020-12-05 13:01:35 +0100

    Proposes a bad fix for the empty list of repositories

--- update.py
+++ update.py
@@ -35,7 +35,7 @@ def getGitHubActivity():
 
     # the reformatting and selection of repositories (matching criteria of latest changes and maximum amount)
     # getting a list of tuples of (days,string)
-    all_repositories = sorted([   [latest_change_in_days(response['updated_at']),make_string(response)]
+    all_repositories = sorted([   [latest_change_in_days(response['updated_at']), make_string(response)]
                                 for response in r
                               ],
                               key=lambda time_and_string:time_and_string[0]
@@ -44,9 +44,9 @@ def getGitHubActivity():
     i = 0
     projects = []
     for repo in all_repositories:
-        if not repo[1] is None and repo[0]<max_recent_activity: # filtering the profile repo
+        if not repo[1] is None and repo[0] < max_recent_activity: # filtering the profile repo
             if (i := i+1) <= max_repos_listed:
-                projects+=[repo[1]]
+                projects += [repo[1]]
 
     # either use the n most recent projects or if no filter applies, use just the most recent
     dynamic_information['projects'] = ' as well as '.join(projects) if len(projects) else all_repositories[0]
@@ -58,22 +58,22 @@ class UpdateREADME:
         
     def readFileContent(self):
         assert 'filename' in self.keys
-        with open(self.information['filename'],'r') as input:
+        with open(self.information['filename'], 'r') as input:
             self.content = input.readlines()
         return self
 
     def writeFileContent(self):
-        with open(self.information['filename'],'w') as output:
+        with open(self.information['filename'], 'w') as output:
             output.write(''.join(self.content))
         return self
 
     def updateContent(self):
         isTrue = lambda x,y: x and y
         equals = lambda x,y: x == y
-        isNone = lambda x: equals(x,None)
-        isNotNone = lambda x: equals(x,not None)
+        isNone = lambda x: equals(x, None)
+        isNotNone = lambda x: equals(x, not None)
         
-        for idx,line in enumerate(self.content):
+        for idx, line in enumerate(self.content):
             line_replacement = line
             if '<!-- ' in line:
                 line_split = line.split('<!-- ')
@@ -87,24 +87,24 @@ class UpdateREADME:
                             break
                         key_value = [e for e in elem.split(' -->')]
                         # even though the number of variables is fixed, i prefer map-reduce over hardcoded indices
-                        if len(key_value) == 2 and reduce(isTrue,map(isNone,[key,value])):
+                        if len(key_value) == 2 and reduce(isTrue, map(isNone, [key, value])):
                             key, value = key_value
-                        elif reduce(equals,map(isNotNone,[key,value])):
+                        elif reduce(equals, map(isNotNone, [key, value])):
                             # if the sliding window does contain 2 different tags, remove it
-                            if reduce(equals,map(lambda x: not equals(*x),zip([key,value],key_value))):
+                            if reduce(equals, map(lambda x: not equals(*x), zip([key, value], key_value))):
                                 key, value = None, None
                                 break
                     
                     # replace the content
-                    if reduce(equals,map(isNotNone,[key,value])):
+                    if reduce(equals, map(isNotNone, [key, value])):
                         for k in self.keys:
                             if k == key:
-                                tag = '<!-- '+key+' -->'
+                                tag = '<!-- ' + key + ' -->'
                                 # an example of regex beeing used (no wildcard necessary, 'value' is escaped)
-                                span = re.search(tag+re.escape(value)+tag,line_replacement).span()
+                                span = re.search(tag + re.escape(value) + tag, line_replacement).span()
                                 # tuple arithmetics
-                                remove = tuple(map(add, span, (len(tag),-len(tag))))
-                                line_replacement = line_replacement[:remove[0]]+self.information[key]+line_replacement[remove[1]:]
+                                remove = tuple(map(add, span, (len(tag), -len(tag))))
+                                line_replacement = line_replacement[:remove[0]]) + str(self.information[key]) + line_replacement[remove[1]:]
             self.content[idx] = line_replacement
         return self
 
